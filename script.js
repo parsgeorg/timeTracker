db46a29d3cfda910cf2be511ae151351d9135c5f
parsgeorg <parsegovgeorgiy@gmail.com>
2016-10-24 15:47:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+var timer;
+function showClock(){
 
+	var	timeControl = document.getElementById("timeControl"), 
+		now = new Date(), 
+		cur_time = now.toLocaleTimeString(),
+		cur_time_split = cur_time.split(":"),
+		hous = +cur_time_split[0]+1,
+		time_res = '';
+		
+		cur_time_split[0] = hous;
+		time_res = cur_time_split.join(':');
+		timeControl.innerHTML = time_res;
+}
+
+function startClock() {
+	timer = window.setInterval('showClock()',1000);
+}
+
+function stopClock() {
+	window.clearInterval(timer);
+	timer = null;
+}
